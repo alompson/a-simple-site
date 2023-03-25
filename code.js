677c5739e9d86933aae1f6e51d5efd67d3124b62
Alompson <alonso963@hotmail.com>
2023-03-25 19:40:31 -0300

    v.0 of my website CV

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+function toggleSkills() {
+    var skillsList = document.getElementById("skills-list");
+    if (skillsList.style.display === "none") {
+        skillsList.style.display = "block";
+    } else {
+        skillsList.style.display = "none";
+    }
+}
